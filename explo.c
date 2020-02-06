1856f553c889235a651052cc5b00322dc451bc58
Xxelll <59962592+Xxelll@users.noreply.github.com>
2020-02-06 16:34:27 +0100

    gestion des lieux

--- explo.c
+++ explo.c
@@ -33,7 +33,30 @@ int main(){
   listeDeLieux[2].destination=0;
 
 
+    char reponse[50];
+    char toulouse[50] = ("toulouse");
+    char montpellier[50] = ("montpellier");
+    char paris [50] = ("paris");
+
+    printf("Voulez vous allez a montpellier, toulouse ou paris ?\n");
+
+    // scan reponse joueur
+    scanf("%s", reponse);
+
+  if (strcmp(reponse,montpellier)==0){
+    printf("Vous allez a montpellier");
+  }
+
+  if (strcmp(reponse,toulouse)==0){
+    printf("Vous allez a toulouse");
+  }
+
+  if (strcmp(reponse,paris)==0){
+    printf("Vous allez a paris");
+  }
 
 
-    return listeDeLieux;
+
+
+    return 0;
 }
