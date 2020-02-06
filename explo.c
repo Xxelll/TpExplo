88d17a2728ea0f882f6b053da9c902ffb693d6bb
Xxelll <59962592+Xxelll@users.noreply.github.com>
2020-02-06 14:04:45 +0100

    Update explo.c

--- explo.c
+++ explo.c
@@ -2,10 +2,26 @@
 #include <string.h>
 #include <stdlib.h>
 
+
+
 int main(){
 
+  int decisionJoueur = 0;
+
+  printf ("Ou voulez vous aller ?\n (1) Toulouse (2) Montpellier.\n");
+  		scanf("%d",&decisionJoueur);
+
+// Decision lieu
+  if(decisionJoueur == 1){
+			printf("\nVous allez a Toulouse \n");
+        }
+
+		if (decisionJoueur == 2){
+			printf("Vous allez a Montpellier\n");
+		}
 
 
-return = 0;
+
+return 0;
 
 }
